29944a5822cafd70a41ceffac2db121c55e154c2
Karine <kah.ohtoshi@gmail.com>
2022-05-24 16:46:24 -0300

    Consumo de API - Login e cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    RodapeComponent,
     EntrarComponent,
     CadastrarComponent,
-    RodapeComponent
+    InicioComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
